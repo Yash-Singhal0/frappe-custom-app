c5a98976add69b1d60ca11bde3133c99a5571669
Yash-Singhal0 <yashsinghalofficial@gmail.com>
2025-04-04 23:38:44 +0000

    commit with custom app and exported fixtures

---
+++ my_custom_app/my_custom_app/doctype/leave_request/leave_request.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, My Custom App and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Leave Request", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ my_custom_app/my_custom_app/doctype/leave_request/test_leave_request.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, My Custom App and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLeaveRequest(FrappeTestCase):
+	pass
